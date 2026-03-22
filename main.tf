1e035461400f6dae6aa6d6d269f90f54cb3b0767
orakanti <orakanti@rptu.de>
2026-03-22 22:21:24 +0100

    changed parameters

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "aws_lb_target_group" "component" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-  deregistration_delay = 60
+  deregistration_delay = 120
   health_check {
-    healthy_threshold = 2
+    healthy_threshold = 3
     interval = 10
-    unhealthy_threshold = 3
-    timeout = 5
+    unhealthy_threshold = 5
+    timeout = 10
     path = "/health"
     port = 80
     matcher = "200-299"
