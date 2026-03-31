156b53e5dfd10ac33c722dfe4e38b4c7b37f7dc4
orakanti <orakanti@rptu.de>
2026-03-31 15:15:30 +0200

    port changed to 8080

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "component" {
   name     = "${local.name}-${var.tags.Component}"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   deregistration_delay = 120
@@ -10,7 +10,7 @@ resource "aws_lb_target_group" "component" {
     unhealthy_threshold = 5
     timeout = 10
     path = "/health"
-    port = 80
+    port = 8080
     matcher = "200-299"
   }
 }
